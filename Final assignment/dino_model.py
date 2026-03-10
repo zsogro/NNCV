5523555a3bac8d99654b69c233c15e7a8b472b42
zsogro <grodvalt.zsolt@gmail.com>
2026-03-10 17:22:45 +0100

    DINOv2 s14

--- Final assignment/dino_model.py
+++ Final assignment/dino_model.py
@@ -3,22 +3,27 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class DINOv2SegmentationModel(nn.Module):
+class Model(nn.Module):
 
-    def __init__(self, num_classes=19):
+    def __init__(self, in_channels=3, num_classes=19):
         super().__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
 
         # Load pretrained DINOv2 backbone
+        # https://github.com/facebookresearch/dinov2
         self.backbone = torch.hub.load(
             'facebookresearch/dinov2',
-            'dinov2_vitb14'
+            'dinov2_vits14'
         )
-
-        embed_dim = 768  # ViT-B embedding size
+        # https://github.com/facebookresearch/dinov2/blob/main/MODEL_CARD.md
+        # ViT-S (38M params): Patch size 14, embedding dimension 384, 6 heads, MLP FFN
+        self.embed_dim = 384  # ViT-S embedding size 
+        self.patch_size = 14  # ViT-S patch size
 
         # Simple segmentation head
         self.seg_head = nn.Sequential(
-            nn.Conv2d(embed_dim, 512, kernel_size=3, padding=1),
+            nn.Conv2d(self.embed_dim, 512, kernel_size=3, padding=1),
             nn.BatchNorm2d(512),
             nn.ReLU(),
 
@@ -26,12 +31,15 @@ class DINOv2SegmentationModel(nn.Module):
             nn.BatchNorm2d(256),
             nn.ReLU(),
 
-            nn.Conv2d(256, num_classes, kernel_size=1)
+            nn.Conv2d(256, self.num_classes, kernel_size=1)
         )
 
     def forward(self, x):
 
-        B, C, H, W = x.shape
+        # Check if the input tensor has the expected number of channels
+        if x.shape[1] != self.in_channels:
+            raise ValueError(f"Expected {self.in_channels} input channels, but got {x.shape[1]}")
+        B, C, H, W = x.shape # batch size, channels, height, width
 
         # Extract patch tokens
         features = self.backbone.forward_features(x)
@@ -43,7 +51,7 @@ class DINOv2SegmentationModel(nn.Module):
         patch_size = int(n_patches ** 0.5)
 
         feat_map = patch_tokens.permute(0, 2, 1).reshape(
-            B, 768, patch_size, patch_size
+            B, self.embed_dim, patch_size, patch_size
         )
 
         # Segmentation head
