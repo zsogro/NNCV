525930cb50d08fd6b919e9ecc402d91ad9da396b
zsogro <grodvalt.zsolt@gmail.com>
2026-03-24 17:48:25 +0100

    Training ood detector, debugging needed.

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 train.py \
+python3 train_ood.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 100 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "dinov3_l16_mlp3_res512_aug" \
+    --experiment-id "OOD Detector v1" \
