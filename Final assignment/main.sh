f829a50f05020248280453f2c73b20c258d0375b
zsogro <grodvalt.zsolt@gmail.com>
2026-03-26 21:15:14 +0100

    CosineAnnealing lr

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 train_ood.py \
+python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 100 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "OOD Detector v1" \
+    --experiment-id "L16_mlp5_CosineAnnealing" \
