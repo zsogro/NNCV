70812655e8b4954d0c5511e13f653e68bf715942
zsogro <grodvalt.zsolt@gmail.com>
2026-03-27 08:44:18 +0100

    Number of epochs=50

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,8 +3,8 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 100 \
+    --epochs 50 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "L16_mlp5_MultiplicativeLR_09" \
+    --experiment-id "L16_mlp3_Multiplicative_09_startEpoch_10" \
