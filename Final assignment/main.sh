9e38919a4fff27619023bc9f20df89ce75425574
zsogro <grodvalt.zsolt@gmail.com>
2026-04-16 10:54:45 +0200

    back to 100 epochs, ood training and predicting works again.

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,7 +3,7 @@ wandb login
 python3 train_ood.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 50 \
+    --epochs 100 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
