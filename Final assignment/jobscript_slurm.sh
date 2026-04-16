e070150a8fa64ff9d6084c1a8c9f7e1567fb7878
zsogro <grodvalt.zsolt@gmail.com>
2026-04-16 09:55:48 +0200

    ood training

--- Final assignment/jobscript_slurm.sh
+++ Final assignment/jobscript_slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
-#SBATCH --partition=gpu_h100
+#SBATCH --partition=gpu_a100
 #SBATCH --time=04:00:00
 
 srun apptainer exec --nv --env-file .env container_v3.sif /bin/bash main.sh
--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,13 +1,12 @@
 wandb login
 
-python3 train.py \
+python3 train_ood.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 50 \
     --lr 0.001 \
     --precision auto \
-    --backbone-train-last-n 1 \
-    --backbone-lr 1e-5 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "L16_All_MLP8_res1024_unfreeze_1" \
+    --experiment-id "CoupledQuadraticSpline_v1" \
+    # --experiment-id "L16_AllMLP8_res1024_scheduler_after_15_epochs" \
